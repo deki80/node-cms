c01277703a734e87f94f6944b3f563e486a0bffc
Dejan <dejan.tomic@yahoo.com>
2020-08-18 21:21:15 +0200

    find single product method and view

--- controllers/products.js
+++ controllers/products.js
@@ -11,6 +11,18 @@ exports.getAllProducts = (req, res, next) => {
     .catch(error => console.log(error))
 }
 
+exports.getSingleProduct = (req, res, next) => {
+  const productId = req.params.productId
+  Product.findByPk(productId)
+    .then(product => {
+      res.render('single', {
+        pageTitle: 'Single Product Page',
+        product: product
+      })
+    })
+    .catch(error => console.log(error))
+}
+
 exports.addProductPage = (req, res, next) => {
   res.send('<form action="/admin/submit-product" method="POST">Title<input type="text" name="title"><br>Image Url<input type="text" name="imageUrl"><br>Price<input type="number" step="0,2" name="price"><br>Description<textarea name="description"></textarea><button type="submit">Submit</button></form')
 }
--- routes/shop.js
+++ routes/shop.js
@@ -5,8 +5,8 @@ const router = express.Router()
 const productsController = require('../controllers/products')
 const pagesController = require('../controllers/pages')
 
-router.get('/products', productsController.getAllProducts)
-
 router.get('/', pagesController.getIndex)
+router.get('/products', productsController.getAllProducts)
+router.get('/products/:productId', productsController.getSingleProduct)
 
 module.exports = router
